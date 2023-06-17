b57091ab6702cfb7db7e948bec130c626143434e
wenhe <wenhelog@gmail.com>
2023-06-17 21:12:14 +0800

    fix: back button overlap on macOS

--- xmcl-keystone-ui/src/composables/windowStyle.ts
+++ xmcl-keystone-ui/src/composables/windowStyle.ts
@@ -11,7 +11,9 @@ export function useWindowStyle() {
     maximized.value = v
   })
   const hideWindowControl = computed(() => state.platform.name === 'osx' || (state.platform.name === 'linux' && state.linuxTitlebar))
+  const platformName = computed(() => state.platform.name)
   return {
+    platformName,
     hideWindowControl,
   }
 }
