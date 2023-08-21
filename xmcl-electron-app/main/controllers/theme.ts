2afe8adcef59b911f33a61118884016fc460f910
CI010 <cijhn@hotmail.com>
2023-08-22 01:39:00 +0800

    WIP

--- xmcl-electron-app/main/controllers/theme.ts
+++ xmcl-electron-app/main/controllers/theme.ts
@@ -5,11 +5,12 @@ import { BaseService } from '@xmcl/runtime'
 
 const expectedValues = ['dark', 'light', 'system']
 export const themePlugin: ControllerPlugin = function (this: ElectronController) {
+  const logger = this.app.getLogger('theme')
   this.app.on('engine-ready', async () => {
     const baseService = await this.app.registry.get(BaseService)
     const settings = await baseService.getSettings()
     if (expectedValues.indexOf(settings.theme) === -1) {
-      this.app.warn(`Cannot set theme source to unexpected value ${settings.theme}. Use dark as default.`)
+      logger.warn(`Cannot set theme source to unexpected value ${settings.theme}. Use dark as default.`)
       nativeTheme.themeSource = 'dark'
     } else {
       nativeTheme.themeSource = settings.theme
